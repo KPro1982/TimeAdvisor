3ed2db2bee793f314f7c66be959d66c0e049b546
Daniel Cravens <daniel_cravens@hotmail.com>
2024-08-03 12:06:17 -0700

    Buttons fixed

--- app.py
+++ app.py
@@ -40,13 +40,42 @@ st.set_page_config(
     )
 
 def ValidateIndex(x):
-    print("Validate: ", x)
+    print("Validate called: x =", x, ": Index =", st.session_state.entryIndex)
+    maxx = len(st.session_state.timeEntries)-1
     if(st.session_state.entryIndex + x < 0):
         st.session_state.entryIndex = 0
-    elif(st.session_state.entryIndex + x > len(st.session_state.timeEntries)-1):
-         st.session_state.entryIndex = len(st.session_state.timeEntries)-1
+    elif(st.session_state.entryIndex + x > maxx):
+         st.session_state.entryIndex = maxx
     else:
        st.session_state.entryIndex = st.session_state.entryIndex + x 
+
+def DisplayReviewTab():
+    col1, col2 = st.columns([1, 1], gap="small")
+    with col1:
+        bcol1, bcol2, bcol3 = st.columns([1, 1, 4], gap="small") 
+        with bcol1:   
+            if(st.button("Prev", key="PREV")):
+                print("PREV pressed")
+                ValidateIndex(-1)
+        with bcol2:
+            if(st.button("Next", key="NEXT")):
+                print("NEXT pressed")
+                ValidateIndex(1)
+        with bcol3:
+            st.write("Entry # ", st.session_state.entryIndex, "out of ", len(st.session_state.timeEntries)-1)
+
+        
+        st.date_input("Date: ", value=st.session_state.timeEntries[st.session_state.entryIndex].Date)
+        match = clientAliases.index(st.session_state.timeEntries[st.session_state.entryIndex].Alias)
+        client_select_alias = st.selectbox(label="Client Alias Selector: ", options=clientAliases, index=match)
+        narrative = st.text_area(label="Narrative: ", value=st.session_state.timeEntries[st.session_state.entryIndex].Narrative)
+        st.number_input("Time Worked: ", min_value=0.0, max_value=8.0, step=0.1, format="%0.1f")
+
+    with col2:
+        print("Col2 Index =", st.session_state.entryIndex)
+        st.text_area("Subject:", value=st.session_state.timeEntries[st.session_state.entryIndex].Subject)
+        st.text_area("Body", value=st.session_state.timeEntries[st.session_state.entryIndex].Body, height=450)
+
 
 
 clientAliases = ['None','Aguilera v. Turner Systems, Inc.', 'Alvarez v. Command Security Services', 'Barnwell v. Gilton Solid Waste Management', 'Bhatia v. Mojio', 'Buksh v. Sixt Rent A Car', 'CAB v. UNITED SITE SERVICES, INC.', 'Casbeer v. Friends of Downtown SLO', 'Castellanos v. Urners, Inc.', 'Clement v. Rescue Mission Alliance', 'Cooper Aerial - PSG Contract Review', 'Crowe v. Alternative Power Generation', 'Deus v. Cuvaison, Inc.', 'Diaz v. Smartlink', 'Ghasemi v Valentia Analytical', 'Gonzalez Davalos v. Nouveau Bakery LLC', 'Gonzalez v. DS Electric, Inc.', 'Hernandez v. TSM Insurance Services', 'Hernandez v. Zarate Foods', 'Hicks v. SSA Group, LLC', 'Jackson v. Mental Health Systems dba TURN', 'Jermane v. Bethany Home Society of San Joaquin', 'Jimenez v. Wade et al ', 'Kolkmann v. Alternative Power Generation', 'Kumar DLSE De Novo Appeals', 'Melendez v. Peters Fruit Farms, Inc.', 'Miller v. Urata & Sons Concrete', 'Olson v. Allen Property Group, Inc.', 'Oracle Anesthesia, Inc. v. Central Valley Anesthesia Partners', 'Page v. Topix Pharmaceuticals', 'Patterson Board v. DCARA', 'PCC v. Fisher Construction Group', 'Raj v. WFP Hospitality II LLC', 'Rentner v. Trimble, Fletchers', 'Rocio Tafoya v. Peters Fruit Farms, Inc.', 'SBM v. Baker', 'Spooner v. Tri-Ced Economic Development Corporation', 'Staedler v. SSA Group, LLC', 'Steve v. Tulare Firestone, Inc.', 'Sweet Adeline v. Tasty Wings', 'TGS Logistics v. PCC Logistics', 'Thomas v. US Tech, Quest Media', 'Turpin v. Sinclair Broadcast Group, Inc.', 'Vaughn v. United Freight Lines', 'Wang et al v. Harris et al.', 'Webb v. Doug Tauzer Construction', 'White v. Andys Produce Market', 'Wood v. Smartlink']
@@ -71,39 +100,11 @@ tab1, tab2, tab3 = st.tabs(["Config", "Review", "Submit"])
 
 
 with tab1:
-    st.subheader("DATAFILE", divider=True)
-    create_button = st.button("Create New")
-    if (create_button):
-        datafileName = st.text_input("Filename", "")
-    append_button = st.button("Append Existing")
-    st.text(datafileName)
     st.subheader("PROCESS EMAIL", divider=True)
     uploaded_emails = st.file_uploader(" ",accept_multiple_files=True, help="Drag and drop emails to process into time entries.")
     if (st.button("Process Email")):
         for email in uploaded_emails:
             st.session_state.timeEntries.append(process_email(email))
 
-
-
 with tab2:
-    col1, col2 = st.columns([1, 1], gap="small")
-    with col1:
-        st.write("Entry # ", st.session_state.entryIndex, "out of ", len(st.session_state.timeEntries)-1)
-        st.date_input("Date: ", value=st.session_state.timeEntries[st.session_state.entryIndex].Date)
-        match = clientAliases.index(st.session_state.timeEntries[st.session_state.entryIndex].Alias)
-        client_select_alias = st.selectbox(label="Client Alias Selector: ", options=clientAliases, index=match)
-        narrative = st.text_area(label="Narrative: ", value=st.session_state.timeEntries[st.session_state.entryIndex].Narrative)
-        st.number_input("Time Worked: ", min_value=0.0, max_value=8.0, step=0.1, format="%0.1f")
-
-        buttoncol1, buttoncol2, buttoncol3 = st.columns([1, 3, 1], gap="small")
-
-        with buttoncol1:
-            st.button("Prev", on_click=ValidateIndex(-1))
-        
-        with buttoncol3:
-            st.button("Next", on_click=ValidateIndex(+1))
-
-    with col2:
-        st.text_area("Subject:", value=st.session_state.timeEntries[st.session_state.entryIndex].Subject)
-        st.text_area("Body", value=st.session_state.timeEntries[st.session_state.entryIndex].Body, height=450)
-
+    DisplayReviewTab()
